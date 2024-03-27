e1ab3165f619ddd001d67e1130732c9c52e021bc
Zupanico <50524719+Zupanico@users.noreply.github.com>
2024-03-27 12:13:02 -0400

    feat:map3

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int main(int argc, char const *argv[])
     SetConsoleOutputCP(CP_UTF8);
     
     game g;
-    g.actualiserMap("map2.txt");
+    g.actualiserMap("map3.txt");
     while (true && !g.getGameOver())
     {
         g.loop();
